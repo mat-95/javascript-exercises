5af5d69bd7ebf090bc697eb418828ea193620e76
mat-95 <98151215+mat-95@users.noreply.github.com>
2025-06-26 16:01:46 +0300

    solve: 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+/**
+ * @param {number[]} arr
+ * @param {number} toRemove
+ * 
+ * @returns {number[]}
+ */
+const removeFromArray = function (arr, ...args) {
+  
+  for (const arg of args) {
+    while (arr.find((number) => number === arg)) {
+      arr.splice(arr.findIndex((number) => number === arg), 1);      
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
