f65b1306c4e5f2b25090e13f94d5c8ad121cac15
mat-95 <98151215+mat-95@users.noreply.github.com>
2025-06-26 17:41:21 +0300

    solve: 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2) || !(typeof num1 === 'number') || !(typeof num2 === 'number')) {
+    return "ERROR";
+  }
+  
+  if (num1 > num2) {
+    const temp = num2;
+    num2 = num1;
+    num1 = temp;    
+  }
 
+  let sum = 0;
+  
+  while (num2 >= num1) {
+    sum += num2;
+    num2--;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
